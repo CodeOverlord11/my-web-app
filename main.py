315ea0cd22a19037c52920c0ecdf26b2ebdab95a
CodeOverlord11 <kashyapsamit1111@gmail.com>
2026-01-29 12:51:40 +0530

    added another webpage

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask,render_template
+
 app=Flask()
 
 @app.route("/")
 def display_index():
     return render_template('index.html')
+
+@app.route("/about")
+def display_about():
+    return render_template('about.html')
